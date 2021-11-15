81058e6cf376c21804f92441d7ae06349f6f4a33
Tamerlan <tsabyrbek@globerce.com>
2021-11-15 16:28:00 +0600

    update

--- asso/urls.py
+++ asso/urls.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
-from django.views.generic.base import RedirectView
-from django.contrib.staticfiles.storage import staticfiles_storage
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from apps.users.views import SignUpView, LogInView
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
     path('api/log_in/', LogInView.as_view(), name='log_in'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/trip/', include('apps.trips.urls', 'trip',)), # new
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    path('api/trip/', include('apps.trips.urls', 'trip',)),
+    path("docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="swagger-schema",
     ),
 ]
