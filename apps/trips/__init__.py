11dab3e6620f41451892d7d5d2c23ed8d298c021
Tamerlan <tsabyrbek@globerce.com>
2021-11-16 10:58:15 +0600

    update

--- apps/trips/__init__.py
+++ apps/trips/__init__.py
@@ -10,6 +10,22 @@ class ServiceNames(TextChoices):
     WASTE_DISPOSAL_MACHINE = "WASTE_DISPOSAL_MACHINE", "Машина для утилизации отходов"
     ASPHALT_PAVER_MACHINE = "ASPHALT_PAVER_MACHINE", "Асфальтоукладчик"
     BULLDOZER = "BULLDOZER", "Бульдозер"
+    DRILLING_EQUIPMENT = "DRILLING_EQUIPMENT", "Буровое оборудование"
+    TRACKED_ALL_TERRAIN_MACHINE = "TRACKED_ALL_TERRAIN_MACHINE", "Гусеничный вездеход"
+    WHEELED_ALL_TERRAIN_MACHINE = "WHEELED_ALL_TERRAIN_MACHINE", "Колесный вездеход"
+    GAS_TANKER = "GAS_TANKER", "Газозаправщик"
+    GENERATOR = "GENERATOR", "Генератор"
+    HYDRAULIC_DRILL = "HYDRAULIC_DRILL", "Гидробур"
+    HYDRODYNAMIC_FLUSHING = "HYDRODYNAMIC_FLUSHING", "Гидродинамическая промывка"
+    GRADER = "GRADER", "Грейдер"
+    CRAWLER_CRANE = "CRAWLER_CRANE", "Гусеничный кран"
+    CRUSHER_MACHINE = "CRUSHER_MACHINE"
+    RINK_MACHINE = "RINK_MACHINE", "Каток"
+    MUNICIPAL_SPECIAL_MACHINE = "MUNICIPAL_SPECIAL_MACHINE", "Коммунальная спецтехника"
+    COMPRESSOR = "COMPRESSOR", "Компрессор"
+    MINI_EXCAVATOR = "MINI_EXCAVATOR", "Мини-экскаватор"
+    MANIPULATOR = "MANIPULATOR", "Манипулятор"
+
 
 class TripStatus(TextChoices):
     REQUESTED = 'REQUESTED', 'Запрошен'
