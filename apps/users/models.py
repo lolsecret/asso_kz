ed416579265398f32f121e61c5f4baf696181b22
Tamerlan <tsabyrbek@globerce.com>
2021-11-16 16:52:54 +0600

    update

--- apps/users/models.py
+++ apps/users/models.py
@@ -57,6 +57,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=20, blank=True, null=True)
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
